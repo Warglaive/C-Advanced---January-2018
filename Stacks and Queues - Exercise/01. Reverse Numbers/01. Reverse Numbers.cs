ba081d1b08953cb5e2275a669d9f7e675fd69ded
warglaive <warglaivegm@gmail.com>
2018-01-23 18:25:43 +0200

    01. Reverse Numbers done

---
+++ Stacks and Queues - Exercise/01. Reverse Numbers/01. Reverse Numbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Reverse_Numbers
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions
+                    .RemoveEmptyEntries)
+                .ToList();
+            var stack = new Stack<int>();
+            for (int i = 0; i <= input.Count - 1; i++)
+            {
+                stack.Push(int.Parse(input[i]));
+            }
+            for (int i = 0; i <= input.Count - 1; i++)
+            {
+                Console.Write(stack.Pop() + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
