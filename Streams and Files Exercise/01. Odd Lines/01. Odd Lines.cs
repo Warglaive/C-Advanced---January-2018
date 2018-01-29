2357317f107f14fda8c88ac44368ac73a11c5fd5
warglaive <warglaivegm@gmail.com>
2018-01-29 14:41:11 +0200

    Streams and Files Exercise to do

---
+++ Streams and Files Exercise/01. Odd Lines/01. Odd Lines.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace _01._Odd_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = new StreamReader("test.txt"))
+            {
+                var length = stream.BaseStream.Length;
+                for (int i = 0; i < length; i++)
+                {
+
+                }
+            }
+        }
+    }
+}
