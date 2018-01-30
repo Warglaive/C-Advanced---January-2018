88d4e14fcad8325bd0430bc46ab8f2b9896ea509
warglaivee <warglaivegm@gmail.com>
2018-01-30 22:40:56 +0200

    BashSoft start

---
+++ BashSoft/BashSoft/IOManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BashSoft
+{
+    public static class IOManager
+    {
+        public static void TraverseDirectory(string path)
+        {
+            OutputWriter.WriteEmptyLine();
+            var initialIdentation = path.Split('\\').Length;
+            var subFolders = new Queue<string>();
+            subFolders.Enqueue(path);
+            while (subFolders.Count != 0)
+            {
+                string currentPath = subFolders.Dequeue();
+                int identation = currentPath.Split('\\').Length - initialIdentation;
+                Console.WriteLine(string.Format("{0}{1}",
+                    new string('-', identation), currentPath));
+
+                foreach (var directoryPath in Directory.GetDirectories(currentPath))
+                {
+                    subFolders.Enqueue(directoryPath);
+                }
+            }
+        }
+    }
+}
---
+++ BashSoft/BashSoft/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BashSoft
+{
+    class Program
+    {
+        static void Main()
+        {
+            IOManager.TraverseDirectory(@"E:\C-Advanced---January-2018\BashSoft");
+        }
+    }
+}
