a5bfee85e7dbb31de59fc962d22061e667b49fe0
warglaivee <warglaivegm@gmail.com>
2018-02-02 18:13:38 +0200

    matrix full

--- Multidimensional Arrays - Exercise/06. Target Practice/06. Target Practice.cs
+++ Multidimensional Arrays - Exercise/06. Target Practice/06. Target Practice.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _06._Target_Practice
 {
@@ -6,7 +7,64 @@ namespace _06._Target_Practice
     {
         public static void Main()
         {
+            var rowsCols = Console.ReadLine()
+                .Split(new[] { ' ' },
+                    StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            var rows = rowsCols[0];
+            var columns = rowsCols[1];
 
+            var matrix = new char[rows, columns];
+
+            var snake = Console.ReadLine().ToCharArray();
+            var shotParams = Console.ReadLine()
+                .Split(new[] { ' ' },
+                    StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var impactRow = shotParams[0];
+            var impactColumn = shotParams[1];
+            var radius = shotParams[2];
+
+            var snakeIndex = 0;
+
+            for (int rowIndex = rows; rowIndex > 0; rowIndex--)
+            {
+                for (int colIndex = columns; colIndex > 0; colIndex--) //right to left
+                {
+                    if (snakeIndex <= snake.Length - 1)
+                    {
+                        matrix[rowIndex - 1, colIndex - 1] = snake[snakeIndex];
+                        snakeIndex++;
+                    }
+                    else
+                    {
+                        colIndex++;
+                        snakeIndex = 0;
+                    }
+                }
+                //left to right
+                rowIndex--;
+                if (rowIndex == 0)
+                {
+                    break;
+                }
+                for (int colIndex = 0; colIndex < columns; colIndex++)
+                {
+                    if (snakeIndex <= snake.Length - 1)
+                    {
+                        matrix[rowIndex - 1, colIndex] = snake[snakeIndex];
+                        snakeIndex++;
+                    }
+                    else
+                    {
+                        colIndex--;
+                        snakeIndex = 0;
+                    }
+                }
+            }
         }
     }
 }
