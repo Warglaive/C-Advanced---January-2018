c121ef953507afa0c3a2420ad9ab296a40081f97
warglaivee <warglaivegm@gmail.com>
2018-02-02 20:05:35 +0200

    06. Target Practice 70/100

--- Multidimensional Arrays - Exercise/06. Target Practice/06. Target Practice.cs
+++ Multidimensional Arrays - Exercise/06. Target Practice/06. Target Practice.cs
@@ -5,6 +5,8 @@ namespace _06._Target_Practice
 {
     public class Program
     {
+        private static int rows;
+        private static int columns;
         public static void Main()
         {
             var rowsCols = Console.ReadLine()
@@ -12,8 +14,8 @@ namespace _06._Target_Practice
                     StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
                 .ToArray();
-            var rows = rowsCols[0];
-            var columns = rowsCols[1];
+            rows = rowsCols[0];
+            columns = rowsCols[1];
 
             var matrix = new char[rows, columns];
 
@@ -30,19 +32,83 @@ namespace _06._Target_Practice
 
             var snakeIndex = 0;
 
+            FillInMatrix(snakeIndex, snake, matrix);
+            Shoot(impactRow, impactColumn, radius, matrix);
+            StartFallin(matrix);
+            Print(matrix);
+        }
+
+        private static void Print(char[,] matrix)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void StartFallin(char[,] matrix)
+        {
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < columns; col++)
+                {
+                    //proverqvame vseki element, ako pod nego e prazno - switchvame
+                    if (matrix[row, col] != ' ' && matrix[row + 1, col] == ' ')
+                    {
+                        var temp = matrix[row, col];
+                        matrix[row + 1, col] = temp;
+                        matrix[row, col] = ' ';
+                        row = 0;
+                        col = 0;
+                    }
+                }
+            }
+        }
+
+        private static void Shoot(int impactRow, int impactColumn, int radius, char[,] matrix)
+        {
+            matrix[impactRow, impactColumn] = ' ';
+            //Calculate radius
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < columns; col++)
+                {
+                    if ((row - impactRow) *
+                        (row - impactRow) +
+                        (col - impactColumn) *
+                        (col - impactColumn)
+                        <= radius * radius)
+                    {
+                        matrix[row, col] = ' ';
+                    }
+                }
+            }
+
+        }
+
+        private static void FillInMatrix(int snakeIndex, char[] snake, char[,] matrix)
+        {
+            var counter = 0;
             for (int rowIndex = rows; rowIndex > 0; rowIndex--)
             {
-                for (int colIndex = columns; colIndex > 0; colIndex--) //right to left
+                if (counter % 2 == 0)
                 {
-                    if (snakeIndex <= snake.Length - 1)
+                    for (int colIndex = columns; colIndex > 0; colIndex--) //right to left
                     {
-                        matrix[rowIndex - 1, colIndex - 1] = snake[snakeIndex];
-                        snakeIndex++;
-                    }
-                    else
-                    {
-                        colIndex++;
-                        snakeIndex = 0;
+                        if (snakeIndex <= snake.Length - 1)
+                        {
+                            matrix[rowIndex - 1, colIndex - 1] = snake[snakeIndex];
+                            snakeIndex++;
+                        }
+                        else
+                        {
+                            colIndex++;
+                            snakeIndex = 0;
+                        }
                     }
                 }
                 //left to right
