8cdf128054b1104991cdee28f81c5cc4e15a57f5
warglaivee <warglaivegm@gmail.com>
2018-02-04 19:09:14 +0200

    up

---
+++ Functional Programming - Exercises/07. Predicate For Names/07. Predicate For Names.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Predicate_For_Names
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var targetLength = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine().Split(new[] { ' ', '\t', '\n', '\r' }
+            , StringSplitOptions.RemoveEmptyEntries);
+            Print(names, n => n.Length <= targetLength);
+        }
+        public static void Print(IEnumerable<string> collection, Func<string, bool> Filter)
+        {
+            Console.WriteLine(string.Join(Environment.NewLine, collection.Where(Filter)));
+        }
+    }
+}
