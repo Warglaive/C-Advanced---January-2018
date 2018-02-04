8ccc2e6f4cff5336a77a7f7fb7d75e24ed92d968
warglaivee <warglaivegm@gmail.com>
2018-02-04 20:08:16 +0200

    examPrep2/4/2018/8:08PM

---
+++ Retake  3 September 2017 ExamPreparation/01. Dangerous Floor/01. Dangerous Floor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Dangerous_Floor
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var boardMatrix = new string[8, 8];
+
+            //fill in matrix
+            for (int row = 0; row < 8; row++)
+            {
+                var line = Console.ReadLine()
+                    .Split(',')
+                    .ToList();
+                for (int col = 0; col < 8; col++)
+                {
+                    boardMatrix[row, col] = line[col];
+                }
+            }
+
+            var input = Console.ReadLine();
+            while (input != "END")
+            {
+                //
+                var inputArgs = input.ToCharArray();
+                var figureType = inputArgs[0];
+                var rowIndex = inputArgs[1];
+                var colIndex = inputArgs[2];
+
+                var finalRow = inputArgs[4];
+                var finalCol = inputArgs[4];
+
+                IsThereApieceOnStartingSquare(figureType,rowIndex,colIndex);
+
+                //
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
