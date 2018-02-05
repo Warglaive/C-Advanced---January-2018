2178a1646d560823963964b88350f6cfbf1ef8cf
warglaive <warglaivegm@gmail.com>
2018-02-05 14:19:04 +0200

    to do

---
+++ EXAM PREPARATION5.2.2018/01. Regeh/01. Regeh.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Regeh
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // [[+]\w+<\d+REGEH - finish it
+        }
+    }
+}
