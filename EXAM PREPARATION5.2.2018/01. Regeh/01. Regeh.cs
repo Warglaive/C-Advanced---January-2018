c7a742125c0ceadda86e3a24f7facf0d7b541238
warglaivee <warglaivegm@gmail.com>
2018-02-05 21:29:25 +0200

    up 0/100

--- EXAM PREPARATION5.2.2018/01. Regeh/01. Regeh.cs
+++ EXAM PREPARATION5.2.2018/01. Regeh/01. Regeh.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace _01._Regeh
 {
@@ -6,7 +9,39 @@ namespace _01._Regeh
     {
         public static void Main()
         {
-            // [[+]\w+<\d+REGEH - finish it
+            var input = Console.ReadLine();
+            var pattern = @"[[+]\w+<\d+REGEH\d+>\w+]";
+            var numPattern = @"\d+";
+            var regex = new Regex(pattern);
+            var numRegex = new Regex(numPattern);
+            var numbersList = new List<int>();
+            var stringResult = string.Empty;
+            foreach (Match Match in regex.Matches(input))
+            {
+                var stringedMatch = Match.ToString();
+                for (int charIndex = 0; charIndex < stringedMatch.Length; charIndex++)
+                {
+                    if (stringedMatch[charIndex].Equals('<'))
+                    {
+                        // take index
+                        foreach (Match digits in numRegex.Matches(stringedMatch))
+                        {
+                            numbersList.Add(int.Parse(digits.Value));
+                            if (numbersList.Sum() > input.Length)
+                            {
+                                var goAtStart = numbersList.Sum() - input.Length;
+                                stringResult += input[goAtStart].ToString();
+                            }
+                            else
+                            {
+                                stringResult += input[numbersList.Sum()].ToString();
+                            }
+                        }
+                        charIndex++;
+                    }
+                }
+            }
+            Console.WriteLine(stringResult);
         }
     }
 }
---
+++ EXAM PREPARATION5.2.2018/02. Knight Game/02. Knight Game.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace _02._Knight_Game
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            if (n < 3)
+            {
+                Console.WriteLine(0);
+                Environment.Exit(0);
+            }
+            var matrix = new string[n, n];
+            //fill in matrix
+            for (int rowIndex = 0; rowIndex < n; rowIndex++)
+            {
+                var input = Console.ReadLine();
+                for (int colIndex = 0; colIndex < n; colIndex++)
+                {
+                    matrix[rowIndex, colIndex] = input[colIndex].ToString();
+                }
+            }
+            //algorithm
+            //check if current knight can attack
+            int currentKnightsInDanger = 0;
+            int maxKnightsInDanger = -1;
+            int mostDangerousKnightRow = 0;
+            int mostDangerousKnightCol = 0;
+            int count = 0;
+            while (true)
+            {
+                for (int rowIndex = 0; rowIndex < n - 2; rowIndex++)
+                {
+                    for (int colIndex = 0; colIndex < n - 2; colIndex++)
+                    {
+                        if (matrix[rowIndex, colIndex] == "K")
+                        {
+                            //2 Rows DOWN - 1 column RIGHT
+                            if ((rowIndex + 2 < n
+                                 && colIndex + 1 < n)
+                                &&
+                                matrix[rowIndex + 2, colIndex + 1] == "K")
+                            {
+                                currentKnightsInDanger++;
+                            }
+                            //2 Rows DOWN - 1 column LEFT
+                            if ((rowIndex + 2 < n
+                                 && colIndex - 1 >= 0)
+                                &&
+                                matrix[rowIndex + 2, colIndex - 1] == "K")
+                            {
+                                currentKnightsInDanger++;
+                            }
+                            //2 Rows UP - 1 column LEFT
+                            if ((rowIndex - 2 >= 0
+                                 && colIndex - 1 >= 0)
+                                &&
+                                matrix[rowIndex - 2, colIndex - 1] == "K")
+                            {
+                                currentKnightsInDanger++;
+
+                            }
+                            //2 Rows UP - 1 column RIGHT
+                            if ((rowIndex - 2 >= 0
+                                 && colIndex + 1 < n)
+                                && matrix[rowIndex - 2, colIndex + 1] == "K")
+                            {
+                                currentKnightsInDanger++;
+                            }
+                            //1 Row UP - 2 columns LEFT
+                            if ((rowIndex - 1 >= 0
+                                 && colIndex - 2 >= 0)
+                                &&
+                                matrix[rowIndex - 1, colIndex - 2] == "K")
+                            {
+                                currentKnightsInDanger++;
+
+                            } // 1 Row Up - 2 columns RIGHT
+                            if (rowIndex - 1 >= 0
+                                && colIndex + 2 < n
+                                && matrix[rowIndex - 1, colIndex + 2] == "K")
+                            {
+                                currentKnightsInDanger++;
+                            }
+                            //1 Row DOWN - 2 columns RIGHT
+                            if ((rowIndex + 1 < n
+                                 && colIndex + 2 < n)
+                                &&
+                                matrix[rowIndex + 1, colIndex + 2] == "K")
+                            {
+                                currentKnightsInDanger++;
+
+                            } //1 Row DOWN - 2 columns LEFT
+                            if ((rowIndex + 1 < n
+                                 && colIndex - 2 >= 0)
+                                &&
+                                matrix[rowIndex + 1, colIndex - 2] == "K")
+                            {
+                                currentKnightsInDanger++;
+                            }
+                        }
+                        if (currentKnightsInDanger > maxKnightsInDanger)
+                        {
+                            maxKnightsInDanger = currentKnightsInDanger;
+                            mostDangerousKnightRow = rowIndex;
+                            mostDangerousKnightCol = colIndex;
+                        }
+                        currentKnightsInDanger = 0;
+                    }
+                }
+                if (maxKnightsInDanger != 0)
+                {
+                    matrix[mostDangerousKnightRow, mostDangerousKnightCol] = "0";
+                    count++;
+                    maxKnightsInDanger = 0;
+                }
+                else
+                {
+                    Console.WriteLine(count);
+                    return;
+                }
+            }
+        }
+    }
+}
