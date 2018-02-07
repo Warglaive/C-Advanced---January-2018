ff366ff143c38db5a4884be42d3375671f745624
warglaivee <warglaivegm@gmail.com>
2018-02-07 17:02:12 +0200

    04. Hospital 100/100

--- EXAM PREPARATION5.2.2018/04. Hospital/04. Hospital.cs
+++ EXAM PREPARATION5.2.2018/04. Hospital/04. Hospital.cs
@@ -8,38 +8,72 @@ namespace _04._Hospital
     {
         public static void Main()
         {
+            var departments = new Dictionary<string, List<string>>();
+            var doctors = new Dictionary<string, List<string>>();
+
             var input = Console.ReadLine().Split(new[] { ' ' }
                     , StringSplitOptions.RemoveEmptyEntries)
                 .ToList();
-            var result = new Dictionary<string, Dictionary<string, Dictionary<string, int>>>();
-            //
-            var roomCount = 0;
             while (input[0] != "Output")
             {
-                var department = input[0];
-                var doctor = input[1] + " " + input[2];
+                var currentDepartment = input[0];
+                var currentDoctor = input[1] + " " + input[2];
                 var patient = input[3];
-
-                if (!result.ContainsKey(department))
+                //add department and patients
+                if (!departments.ContainsKey(currentDepartment))
                 {
-                    result.Add(department, new Dictionary<string, Dictionary<string, int>>());
+                    departments.Add(currentDepartment, new List<string>());
                 }
-
-                if (!result[department].ContainsKey(doctor))
+                departments[currentDepartment].Add(patient);
+                //add doctor and patients
+                if (!doctors.ContainsKey(currentDoctor))
                 {
-                    result[department].Add(doctor, new Dictionary<string, int>());
+                    doctors.Add(currentDoctor, new List<string>());
                 }
-
-                if (!result[department][doctor].ContainsKey(patient))
-                {
-                    result[department][doctor].Add(patient, roomCount);
-                }
-
+                doctors[currentDoctor].Add(patient);
                 input = Console.ReadLine().Split(new[] { ' ' }
                         , StringSplitOptions.RemoveEmptyEntries)
                     .ToList();
             }
             var printCommand = Console.ReadLine();
+            while (printCommand != "End")
+            {
+                var printCommandArgs = printCommand.Split(' ').ToList();
+                var expectedOutput = printCommandArgs[0];
+                if (printCommandArgs.Count > 1)
+                {
+                    int roomNumber;
+
+                    if (int.TryParse(printCommandArgs[1], out roomNumber))
+                    {
+                        var skipCount = 3 * (roomNumber - 1);
+                        foreach (var patients in departments[expectedOutput]
+                            .Skip(skipCount)
+                            .Take(3).OrderBy(x => x))
+                        {
+                            Console.WriteLine(patients);
+                        }
+                    }
+                    else
+                    {
+                        var doctor = expectedOutput + " " + printCommandArgs[1];
+                        foreach (var currentDoctor in doctors[doctor]
+                            .OrderBy(x => x))
+                        {
+                            Console.WriteLine(currentDoctor);
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var currentDepartment in departments[expectedOutput])
+                    {
+                        Console.WriteLine(currentDepartment);
+                    }
+                }
+                printCommand = Console.ReadLine();
+            }
         }
     }
 }
+
