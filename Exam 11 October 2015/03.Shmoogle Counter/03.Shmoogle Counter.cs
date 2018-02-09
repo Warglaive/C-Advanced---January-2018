484971ea965a9c8c1e3ebe6318793a71d7798345
warglaivee <warglaivegm@gmail.com>
2018-02-09 17:44:45 +0200

    03.Shmoogle Counter 100/100

---
+++ Exam 11 October 2015/03.Shmoogle Counter/03.Shmoogle Counter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _03.Shmoogle_Counter
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var doublesResult = new List<string>();
+            var intResult = new List<string>();
+            var integerPattern = @"int [a-z]{1}[a-zA-Z]*";
+            var doublePattern = @"double [a-z]{1}[a-zA-Z]*";
+            var intRegex = new Regex(integerPattern);
+            var regex = new Regex(doublePattern);
+            var input = Console.ReadLine();
+            while (input != "//END_OF_CODE")
+            {
+                foreach (Match letter in regex.Matches(input))
+                {
+                    var splitDouble = letter.Value.Split(' ');
+                    doublesResult.Add(splitDouble[1]);
+                }
+                foreach (Match intVarName in intRegex.Matches(input))
+                {
+                    var splitInteger = intVarName.Value.Split(' ');
+                    intResult.Add(splitInteger[1]);
+                }
+
+                input = Console.ReadLine();
+            }
+            if (doublesResult.Count > 0)
+            {
+                Console.WriteLine($"Doubles: {string.Join(", ", doublesResult.OrderBy(x => x))}");
+            }
+            else
+            {
+                Console.WriteLine("Doubles: None");
+            }
+            if (intResult.Count > 0)
+            {
+                Console.WriteLine($"Ints: {string.Join(", ", intResult.OrderBy(x => x))}");
+            }
+            else
+            {
+
+                Console.WriteLine("Ints: None");
+            }
+        }
+    }
+}
